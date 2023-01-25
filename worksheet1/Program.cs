89b1ec901c8908ece7d5d8ee7fed67921ddfd2a0
s00236260 <s00236260@LAB-D2039-24>
2023-01-25 09:46:09 +0000

    Add project files.

---
+++ worksheet1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace worksheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
