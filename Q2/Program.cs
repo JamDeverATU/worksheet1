3b45038d3d5aa88d9c7ae8472d9c0a1129bb3343
James Mc Cafferty Devers <S00236260@atu.ie>
2023-01-30 10:48:31 +0000

    adaadadadad

---
+++ Q2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace q2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountOddNumbers();
+            AverageMinMax();
+        }
+
+        static void CountOddNumbers()
+        {
+            Console.WriteLine("Enter 5 integers:");
+            int oddCount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+            Console.WriteLine("Number of odd integers: " + oddCount);
+        }
+
+
+        static void AverageMinMax()
+        {
+            int num, sum = 0, smallest = int.MaxValue, largest = int.MinValue;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter an integer: ");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+
+            Console.WriteLine("Average: " + (double)sum / 5);
+            Console.WriteLine("Smallest: " + smallest);
+            Console.WriteLine("Largest: " + largest);
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountOddNumbers();
+            Console.WriteLine();
+            PrintTriangleUpsideDown();
+        }
+
+        static void CountOddNumbers()
+        {
+            {
+                PrintTriangle(5);
+                Console.ReadLine();
+            }
+
+            static void PrintTriangle(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("# ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void PrintTriangleUpsideDown()
+        {
+
+            {
+                PrintTriangle(5);
+                Console.ReadLine();
+            }
+
+            static void PrintTriangle(int n)
+            {
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("# ");
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
+
+
+        }
+    }
+}
+
